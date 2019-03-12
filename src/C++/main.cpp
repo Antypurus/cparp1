37348375364a3f0303ca5a601e5f55e1a4da98bd
Tiago Magalhães <up201607931@fe.up.pt>
2019-03-12 23:31:28 +0000

    fuck polymorphism

--- src/C++/main.cpp
+++ src/C++/main.cpp
@@ -4,9 +4,9 @@
 int main()
 {
 	int a[]{ 1,2,3,4 };
-	matrix<float>* m = new serial_matrix<float>(2000, 2000);
-	matrix<float>* n = new serial_matrix<float>(2000, 2000);
-	auto b = ((*m) * (*n));
+	serial_matrix<float>m(2000, 2000);
+	serial_matrix<float>n(2000, 2000);
+	auto b = ((m) * (n));
 	int val = b->get_val(1, 1);
 	printf("%d\n", val);
 }
--- src/C++/serial_matrix.hpp
+++ src/C++/serial_matrix.hpp
@@ -11,9 +11,12 @@ private:
     T* m_matrix; //array containing the actual matrix data
 public:
     //interaface methods
-    std::unique_ptr<matrix<T>> operator*(const matrix<T>& matrix)const override;
+    std::unique_ptr<matrix<T>> operator*(const matrix<T>* matrix)const override;
     T& get_val(const size_t i,const size_t j) override;
 	const T& get(const size_t i, const size_t j)const override;
+
+	//non polimorfic operator
+	std::unique_ptr<serial_matrix<T>> operator*(const serial_matrix<T>& matrix)const;
 
     //class methods
     serial_matrix(const size_t width,const size_t height);
@@ -25,14 +28,14 @@ public:
 template<typename T>
 serial_matrix<T>::serial_matrix(const size_t width,const size_t height):matrix<T>(width,height)
 {
-	this->m_matrix = new T[width*height]();
+	this->m_matrix = (T*)malloc(width * height * sizeof(T));
 	std::fill(this->m_matrix, this->m_matrix + (width*height), T());
 }
 
 template<typename T>
 serial_matrix<T>::serial_matrix(const size_t width, const size_t height, const T matrix_data[]):matrix<T>(width, height)
 {
-	this->m_matrix = new T[width * height]();
+	this->m_matrix = (T*)malloc(width * height * sizeof(T));
 	std::copy(matrix_data, matrix_data+(width*height),this->m_matrix);
 }
 
@@ -45,26 +48,30 @@ inline serial_matrix<T>::serial_matrix(const size_t width, const size_t height, 
 template<typename T>
 serial_matrix<T>::~serial_matrix()
 {
-	delete this->m_matrix;
+	free(this->m_matrix);
 }
 
 template<typename T>
-inline std::unique_ptr<matrix<T>> serial_matrix<T>::operator*(const matrix<T>& matrix)const
+inline std::unique_ptr<matrix<T>> serial_matrix<T>::operator*(const matrix<T>* matrix)const
 {
-	T* res = new T[matrix.get_width() * this->m_height]();
-	std::fill(res, res + (this->m_width*this->m_height), T());
+	T* res = (T*)malloc(matrix->get_width() * this->m_height * sizeof(T));
+	std::fill(res, res + (matrix->get_width() * this->m_height), T());
 
 	//calculate the matrix - this version is bad for cache
 	{
 		//auto start = GetTickCount();
 
-		for (size_t matrix_A_line = 0; matrix_A_line < this->m_height; ++matrix_A_line)
+		size_t mA_height = this->m_height;
+		size_t mB_height = matrix->get_height();
+		size_t mB_width = matrix->get_width();
+
+		for (size_t matrix_A_line = 0; matrix_A_line < mA_height; ++matrix_A_line)
 		{
-			for (size_t matrix_B_col = 0; matrix_B_col < matrix.get_width(); ++matrix_B_col)
+			for (size_t matrix_B_line = 0; matrix_B_line < mB_height; ++matrix_B_line)
 			{
-				for (size_t matrix_B_line = 0; matrix_B_line < matrix.get_height(); ++matrix_B_line)
+				for (size_t matrix_B_col = 0; matrix_B_col < mB_width; ++matrix_B_col)
 				{
-					res[matrix_A_line * this->get_height() + matrix_B_col] += this->get(matrix_A_line, matrix_B_line) * matrix.get(matrix_B_line, matrix_B_col);
+					res[matrix_A_line * this->get_height() + matrix_B_col] += this->get(matrix_A_line, matrix_B_line) * matrix->get(matrix_B_line, matrix_B_col);
 				}
 			}
 		}
@@ -74,7 +81,7 @@ inline std::unique_ptr<matrix<T>> serial_matrix<T>::operator*(const matrix<T>& m
 		//printf("Time Spent:%d", (end - start));
 	}
 
-	return std::make_unique<serial_matrix<T>>(matrix.get_width(),this->m_height,std::move(res));
+	return std::make_unique<serial_matrix<T>>(matrix->get_width(),this->m_height,std::move(res));
 }
 
 template<typename T>
@@ -87,4 +94,37 @@ template<typename T>
 inline const T& serial_matrix<T>::get(const size_t line, const size_t column)const
 {
 	return this->m_matrix[line * this->m_width + column];
+}
+
+template<typename T>
+inline std::unique_ptr<serial_matrix<T>> serial_matrix<T>::operator*(const serial_matrix<T>& matrix) const
+{
+	T* res = (T*)malloc(matrix.get_width() * this->m_height * sizeof(T));
+	std::fill(res, res + (matrix.get_width() * this->m_height), T());
+
+	//calculate the matrix - this version is bad for cache
+	{
+		//auto start = GetTickCount();
+
+		size_t mA_height = this->m_height;
+		size_t mB_height = matrix.get_height();
+		size_t mB_width = matrix.get_width();
+
+		for (size_t matrix_A_line = 0; matrix_A_line < mA_height; ++matrix_A_line)
+		{
+			for (size_t matrix_B_line = 0; matrix_B_line < mB_height; ++matrix_B_line)
+			{
+				for (size_t matrix_B_col = 0; matrix_B_col < mB_width; ++matrix_B_col)
+				{
+					res[matrix_A_line * this->get_height() + matrix_B_col] += this->get(matrix_A_line, matrix_B_line) * matrix.get(matrix_B_line, matrix_B_col);
+				}
+			}
+		}
+
+		//auto end = GetTickCount();
+
+		//printf("Time Spent:%d", (end - start));
+	}
+
+	return std::make_unique<serial_matrix<T>>(matrix.get_width(), this->m_height, std::move(res));
 }
