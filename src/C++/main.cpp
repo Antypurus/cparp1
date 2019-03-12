0a4dc1f15becdfe88fb12ed28ee3454b2a849c75
Tiago Magalhães <up201607931@fe.up.pt>
2019-03-12 10:02:25 +0000

    updated gitignore

--- src/C++/main.cpp
+++ src/C++/main.cpp
@@ -4,7 +4,9 @@
 int main()
 {
 	int a[]{ 1,2,3,4 };
-	serial_matrix<int> m(2, 2, a);
-	int val = m.get_val(1, 1);
+	matrix<int>* m = new serial_matrix<int>(2, 2);
+	matrix<int>* n = new serial_matrix<int>(2, 2);
+	auto b = ((*m)*(*n));
+	int val = b->get_val(1, 1);
 	printf("%d\n", val);
 }
--- src/C++/matrix.hpp
+++ src/C++/matrix.hpp
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <memory>
 
 template<typename T>
 class matrix
@@ -9,9 +10,9 @@ protected:
 public:
     //interface methods
     matrix(const size_t width,const size_t height):m_width(width),m_height(height){};
-    virtual matrix<T>* operator*(const matrix<T>& matrix) = 0;
+    virtual std::unique_ptr<matrix<T>> operator*(const matrix<T>& matrix) = 0;
     virtual T& get_val(const size_t i,const size_t j) = 0;
-    virtual ~matrix()=0;
+	virtual ~matrix() {};
 
     //always the same methods
     size_t get_width(){return this->m_width;}
