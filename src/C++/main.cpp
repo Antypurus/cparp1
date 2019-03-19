00e3e6b3d332ae2eecd1cf54d16c8cf5a1539e7b
up201607931 <up201607931@FEUP-I003D02>
2019-03-19 09:44:45 +0000

    added papi ftw

--- src/C++/main.cpp
+++ src/C++/main.cpp
@@ -1,14 +1,19 @@
 #include "serial_matrix.hpp"
+#include "papi_interface.hpp"
 #include <stdio.h>
 #include <cstdlib>
 
 int main(int argc,char** argv)
 {
+	int events = papi::start_using();
 	unsigned int matrix_size = atoi(argv[1]);
 	unsigned char algorithm = atoi(argv[2]);
 	serial_matrix<float>m(matrix_size, matrix_size, algorithm);
 	serial_matrix<float>n(matrix_size, matrix_size, algorithm);
+	papi::start_measure(events);
 	auto b = (m * n);
+	papi::stop_measure(events);
 	int val = b->get_val(1, 1);
 	printf("%d\n", val);
+	papi::stop_using(events);
 }
---
+++ src/C++/papi_interface.cpp
@@ -0,0 +1,91 @@
+#include "papi_interface.hpp"
+#include <papi.h>
+#include <cstdlib>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+namespace papi{
+void handle_error (int retval)
+{
+  printf("PAPI error %d: %s\n", retval, PAPI_strerror(retval));
+  exit(1);
+}
+
+void init_papi() {
+  int retval = PAPI_library_init(PAPI_VER_CURRENT);
+  if (retval != PAPI_VER_CURRENT && retval < 0) {
+    printf("PAPI library version mismatch!\n");
+    exit(1);
+  }
+  if (retval < 0) handle_error(retval);
+
+  std::cout << "PAPI Version Number: MAJOR: " << PAPI_VERSION_MAJOR(retval)
+            << " MINOR: " << PAPI_VERSION_MINOR(retval)
+            << " REVISION: " << PAPI_VERSION_REVISION(retval) << "\n";
+}
+
+int start_using()
+{
+    int ret;
+	int EventSet = PAPI_NULL;
+
+	ret = PAPI_library_init( PAPI_VER_CURRENT );
+	if ( ret != PAPI_VER_CURRENT )
+		std::cout << "FAIL" << endl;
+
+
+	ret = PAPI_create_eventset(&EventSet);
+		if (ret != PAPI_OK) cout << "ERRO: create eventset" << endl;
+
+
+	ret = PAPI_add_event(EventSet,PAPI_L1_DCM );
+	if (ret != PAPI_OK) cout << "ERRO: PAPI_L1_DCM" << endl;
+
+
+	ret = PAPI_add_event(EventSet,PAPI_L2_DCM);
+	if (ret != PAPI_OK) cout << "ERRO: PAPI_L2_DCM" << endl;
+
+    return EventSet;
+}
+
+void stop_using(int EventSet)
+{
+    int ret;
+    ret = PAPI_remove_event( EventSet, PAPI_L1_DCM );
+		if ( ret != PAPI_OK )
+			std::cout << "FAIL remove event" << endl; 
+
+		ret = PAPI_remove_event( EventSet, PAPI_L2_DCM );
+		if ( ret != PAPI_OK )
+			std::cout << "FAIL remove event" << endl; 
+
+		ret = PAPI_destroy_eventset( &EventSet );
+		if ( ret != PAPI_OK )
+			std::cout << "FAIL destroy" << endl;
+}
+
+void start_measure(int EventSet)
+{
+    int ret;
+		ret = PAPI_start(EventSet);
+		if (ret != PAPI_OK) cout << "ERRO: Start PAPI" << endl;
+}
+
+void stop_measure(int EventSet)
+{
+    int ret;
+    long long values[2];
+
+    ret = PAPI_stop(EventSet, values);
+  		if (ret != PAPI_OK) cout << "ERRO: Stop PAPI" << endl;
+  		printf("L1 DCM: %lld \n",values[0]);
+  		printf("L2 DCM: %lld \n",values[1]);
+
+		ret = PAPI_reset( EventSet );
+		if ( ret != PAPI_OK )
+			std::cout << "FAIL reset" << endl; 
+}
+
+}
